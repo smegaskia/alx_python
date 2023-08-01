76f6384ada4554dcb496ea56985379b6ea5e300f
Salahuddeen-Mai-Gaskiya <125193859+smegaskia@users.noreply.github.com>
2023-08-01 20:20:52 +0100

    Update 5-print_comb2.py

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,3 +1,3 @@
+#where {i:02d} means 2digit. 'end' means end with ','
 for i in range(100):
     print(f"{i:02d}", end=", " if i < 99 else "\n")
-#where {i:02d} means 2digit. 'end' means end with ','
