70cf5e1514bf57d587e506918f283085f3b5dc8f
Salahuddeen-Mai-Gaskiya <125193859+smegaskia@users.noreply.github.com>
2023-08-01 15:05:34 +0100

    Update 3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,17 +1,12 @@
-#!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 #Last digit of 4205 is 5 and is less than 6 and not 0
-num = str(number)
-if num[-1:] > '5':
-    #print goes here!
-    print(f'Last digit of {number} is {str(number)[-1:]} and is greater than 5')
-elif num[-1:] == '0':
-        #Zero printing goes here!
-    print(f'Last digit of {number} is {str(number)[-1:]} and is 0')
-elif num[-1:] < '6':
-    if num[-1:] != '0':
-        #last print goes here! 
-        print(f'Last digit of {number} is {str(number)[-1:]} and is less than 6 and not 0')
-        
+last_digit = abs(number) % 10
+
+if last_digit > 5:
+    print(f'Last digit of {number} is {last_digit} and is greater than 5')
+elif last_digit == 0:
+    print(f'Last digit of {number} is {last_digit} and is 0')
+else:
+    print(f'Last digit of {number} is {last_digit} and is less than 6 and not 0')
